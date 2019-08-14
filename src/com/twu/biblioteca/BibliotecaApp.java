939d90e1f9cc037900dcbc2f17167336fd192101
Ines Ondo <inesondo@MacBook-Pro-de-Ines.local>
2019-08-14 08:43:53 +0200

    display option menu + exception error

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,20 +1,40 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Arrays;
 
 public class BibliotecaApp {
     private final PrintStream writer;
+    private final BufferedReader reader;
     private final Library library;
 
-    public BibliotecaApp(PrintStream writer, Library library) {
+    public BibliotecaApp(PrintStream writer, BufferedReader reader, Library library) {
         this.writer = writer;
+        this.reader = reader;
         this.library = library;
     }
 
     public void run(){
         printGreeting();
-        printLibraryBooks();
+        printMenu();
+        try {
+            String line = reader.readLine();
+            if(line == null) {
+                return;
+            }
+            int optionChosen = parseOption(line);
+            switch (optionChosen) {
+                case 1:
+                    printLibraryBooks();
+                    break;
+                default:
+                    writer.println("Please select a valid option!");
+            }
+        } catch (IOException e) {
+        }
     }
 
     private void printGreeting() {
@@ -26,6 +46,19 @@ public class BibliotecaApp {
         }
     }
 
+    private int parseOption(String line) {
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException ex) {
+            return -1;
+        }
+    }
+
+    private void printMenu() {
+        writer.println("Choose an option");
+        writer.println("1. List of books");
+    }
+
     public static void main(String[] args) {
         Library library = new Library(Arrays.asList(
                 new Book("Java for beginners", "Oracle", 2000),
@@ -35,7 +68,7 @@ public class BibliotecaApp {
                 new Book("jUnit for beginners", "Tester", 2005)
         ));
 
-        BibliotecaApp biblioteca = new BibliotecaApp(System.out, library);
+        BibliotecaApp biblioteca = new BibliotecaApp(System.out, new BufferedReader(new InputStreamReader(System.in)), library);
         biblioteca.run();
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -6,7 +6,9 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
@@ -14,6 +16,9 @@ import static org.junit.Assert.*;
 public class BibliotecaAppTest {
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     PrintStream writer = new PrintStream(output);
+    BufferedReader reader = new BufferedReader(new StringReader("1\n"));
+    List emptyLibrary = new ArrayList();
+
     @Test
     public void testPrintingListOfBooks() {
         Library books = new Library(Arrays.asList(
@@ -23,8 +28,10 @@ public class BibliotecaAppTest {
                 new Book("React for beginners", "React", 2016),
                 new Book("jUnit for beginners", "Tester", 2005)
         ));
-        BibliotecaApp biblioteca = new BibliotecaApp(writer, books);
+        BibliotecaApp biblioteca = new BibliotecaApp(writer, reader, books);
         String printedInfo = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!\n" +
+                "Choose an option\n" +
+                "1. List of books\n" +
                 "Java for beginners | Oracle | 2000\n" +
                 "JavaScript for beginners | ES | 2006\n" +
                 "Node for beginners | Node | 2007\n" +
@@ -33,5 +40,43 @@ public class BibliotecaAppTest {
         biblioteca.run();
         assertThat(new String(output.toByteArray()), equalTo(printedInfo));
 
+    }
+
+    @Test
+    public void testDisplayMenu() {
+        Library books = new Library(emptyLibrary);
+        BufferedReader reader = new BufferedReader(new StringReader(""));
+        BibliotecaApp biblioteca = new BibliotecaApp(writer, reader, books);
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!\n" +
+                "Choose an option\n" +
+                "1. List of books\n";
+        biblioteca.run();
+        assertThat(new String(output.toByteArray()), equalTo(expectedMessage));
+    }
+
+    @Test
+    public void testDisplayErrorOutOfRangeNumber() {
+        Library books = new Library(emptyLibrary);
+        BufferedReader reader = new BufferedReader(new StringReader("10\n"));
+        BibliotecaApp biblioteca = new BibliotecaApp(writer, reader, books);
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!\n" +
+                "Choose an option\n" +
+                "1. List of books\n" +
+                "Please select a valid option!\n";
+        biblioteca.run();
+        assertThat(new String(output.toByteArray()), equalTo(expectedMessage));
+    }
+
+    @Test
+    public void testDisplayErrorNonNumeric() {
+        Library books = new Library(emptyLibrary);
+        BufferedReader reader = new BufferedReader(new StringReader("one\n"));
+        BibliotecaApp biblioteca = new BibliotecaApp(writer, reader, books);
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!\n" +
+                "Choose an option\n" +
+                "1. List of books\n" +
+                "Please select a valid option!\n";
+        biblioteca.run();
+        assertThat(new String(output.toByteArray()), equalTo(expectedMessage));
     }
 }
